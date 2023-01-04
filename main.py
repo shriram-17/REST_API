1dc369459bcf4cc2d5fbcd60f1451b4316cff23a
shriram-17 <76253109+shriram-17@users.noreply.github.com>
2023-01-04 22:26:22 +0530

    Added Some Requests

--- main.py
+++ main.py
@@ -1,24 +1,79 @@
 from flask import Flask,request
-from flask_restful import Api,Resource,reqparse
+from flask_restful import Api,Resource, abort,reqparse,fields,marshal_with
+from flask_sqlalchemy import SQLAlchemy
 
 app=Flask(__name__)
 api=Api(app)
 
+app.config['SQLALCHEMY_DATABASE_URI']='sqlite:///database.db'
+db =SQLAlchemy(app)
+
+
+class VideoModel(db.Model):
+    id=db.Column(db.Integer,primary_key=True)
+    name=db.Column(db.String(100),nullable=False)
+    views=db.Column(db.Integer,nullable=False)
+    likes=db.Column(db.Integer,nullable=False)
+    
+    def __repr__(self):
+        return f"Video(name={self.name},views={self.views},likes={self.likes})"
+
+#db.create_all()
 video_put_args=reqparse.RequestParser()
 
 video_put_args.add_argument("name",type=str,help="Name of the Video is Required",required=True)
 video_put_args.add_argument("likes",type=int,help="likes of the Video is Required",required=True)
 video_put_args.add_argument("views",type=int,help="Views of the Video is Required ",required=True)
 
-videos={}
+video_update_args=reqparse.RequestParser()
 
+video_update_args.add_argument("name",type=str,help="Name of the Video is Required")
+video_update_args.add_argument("likes",type=int,help="likes of the Video is Required")
+video_update_args.add_argument("views",type=int,help="Views of the Video is Required")
+
+resource_fields={
+    'id':fields.Integer,
+    'name':fields.String,
+    'views':fields.Integer,
+    'likes':fields.Integer
+}
+        
 class Video(Resource):
+    @marshal_with(resource_fields)
     def get(self,video_id):
-        return videos[video_id]
+        result=VideoModel.query.filter_by(id=video_id).first()
+        if not result:
+            abort(404,message="Wrong Id Enter Again")
+        return result
+    
+    @marshal_with(resource_fields)
     def post(self,video_id):
         args=video_put_args.parse_args()
-        return {video_id:args}
- 
+        result=VideoModel.query.filter_by(id=video_id).first()
+        if result:
+             abort(409,message="Video id taken")
+        Video=VideoModel(id=video_id,name=args['name'],views=args['views'],likes=args['likes'])
+        db.session.add(Video)
+        db.session.commit()
+        return Video,201
+    
+    @marshal_with(resource_fields)
+    def patch(self,video_id):
+        args=video_update_args.parse_args()
+        result=VideoModel.query.filter_by(id=video_id).first()
+        if not result:
+             abort(404,message="Cannot Update The Video Id")
+        if args['name']:
+            result.name=args['name']
+        if args['views']:
+            result.views=args['views']
+        if args['likes']:
+            result.likes=args['likes']
+        
+        db.session.commit()
+        return result
+         
+    
 api.add_resource(Video,"/video/<int:video_id>")
   
 if __name__ == "__main__":
--- test.py
+++ test.py
@@ -1,7 +1,21 @@
 
 import requests
 
+data=[
+    {"likes":10111,"name":"Halo 3 is Awesome","views":10000},
+    {"likes":11000,"name":"Why is Node is better than Flask","views":1000},
+    {"likes":10000,"name":"Essay on War and Peace","views":10000}
+]
 BASE ="http://127.0.0.1:5000/"
 
-response=requests.post(BASE+"video/1",{"likes":10})
-print(response.json())
+for i in range(len(data)):
+    response=requests.post(BASE+"video/" +str(i),data[i])
+    print(response.json()) 
+
+input()    
+reponse =requests.get(BASE+"video/4")
+print(reponse.json())
+
+input()
+reponse=requests.patch(BASE+"video/1",{"likes":5000})
+print(reponse.json())
